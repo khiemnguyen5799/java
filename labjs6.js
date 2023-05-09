78f21c7243c9fcd5e778b6d5bab307a3bba8a760
khiemnguyen5799 <khiemnguyen5799@gmail.com>
2023-05-09 18:28:00 +0700

    lab6

---
+++ labjs6.js
@@ -0,0 +1,21 @@
+
+// Lab 6. Xử lý chuỗi
+
+//  dữ liệu 1
+/*
+const printForecast = function() {
+  const array =  [17, 21, 23];
+  return `... ${array[0]}ºC in 1 day ... ${array[1]}ºC in 2 days ... ${array[2]}ºC in 3 days ...`;
+};
+
+console.log(printForecast());
+*/
+
+// dữ liệu 2
+
+const printForecast = function() {
+  const array = [12, 5, -5, 0, 4];
+  return `... ${array[0]}ºC in 1 day ... ${array[1]}ºC in 2 days ... ${array[2]}ºC in 3 days ...${array[3]}ºC in 4 days ...${array[4]}ºC in 5 days ...`;
+};
+
+console.log(printForecast());
