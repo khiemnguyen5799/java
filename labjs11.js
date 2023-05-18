01d11f9123ad010392d5bd2cf44d122911a67904
khiemnguyen5799 <khiemnguyen5799@gmail.com>
2023-05-18 18:51:11 +0700

    sửa lab 11

--- labjs11.js
+++ labjs11.js
@@ -3,29 +3,32 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   numberOfVotes: new Array(4).fill(0),
 
-  registerNewAnswer() {
-    let prom = Number(
-      prompt(`${this.question}?/n${this.options}/n(Write option number)`)
+  registerNewAnswer: function () {
+    const prom = Number(
+      prompt(`${this.question}\n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}\n(Write option number)`)
     );
 
-    while (isNaN(prom) || prom < 0 || prom > 3) {
+    if (!isNaN(prom) && prom >= 0 && prom <= 3) {
+      this.numberOfVotes[prom]++;
+    }else{
       alert("Câu trả lời không hợp lệ");
-      prom = prompt(
-        `${this.question}?/n${this.options}/n(Write option number)`
-      );
     }
-    // this.numberOfVotes[prom]++;
   },
-  displayResults(type = "string") {
-    if (type === "string") {
+  displayResults: function (type) {
+    if (type === 'string') {
       console.log(`Poll result are${this.numberOfVotes.join(',')}`);
-    } else if (type === "array") {
+    } else if (type === 'array') {
       console.log(this.numberOfVotes);
     }
   }
 };
-const btnAnswer = document.getElementById("btn-answer");
+btnAnswer = document.getElementById("btn-answer");
+
 btnAnswer.addEventListener('click', function () {
-  console.log( [5, 2, 3]);
-})
+  poll.registerNewAnswer();
+  poll.displayResults('array');
+  poll.displayResults('string');
+});
+
+
 
